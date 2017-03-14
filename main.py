598e519192634473b7e078eb41726d46dca417ff
Thinkman Wang <thinkman@function-hz.com>
2017-03-15 02:40:11 +0800

    update

--- main.py
+++ main.py
@@ -7,5 +7,8 @@ from thinkutils.datetime.datetime import *
 logger = setup_custom_logger()
 
 if __name__ == '__main__':
+    # date time test
     logger.info("Test")
     logger.info("date: %d" % (get_timestamp()))
+    logger.info(get_current_time_str())
+    logger.info(timestamp2str(get_timestamp()))
--- thinkutils/datetime/datetime.py
+++ thinkutils/datetime/datetime.py
@@ -2,3 +2,13 @@ import time
 
 def get_timestamp():
     return int(time.time())
+
+def get_current_time_str():
+    temp = time.localtime(time.time())
+    szTime = time.strftime("%Y-%m-%d %H:%M:%S", temp)
+    return szTime
+
+def timestamp2str(tt):
+    t1 = time.localtime(float(tt))
+    t2 = time.strftime("%Y-%m-%d %H:%M:%S",t1)
+    return t2;
