52b3da71f3943887e9ff1de262e21d50e4676620
Thinkman Wang <thinkman@function-hz.com>
2017-03-15 13:57:26 +0800

    fxxk

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-import sys
-
-from thinkutils.log.log import *
+from thinkutils.common_utils.think_hashlib import *
 from thinkutils.datetime.datetime import *
+from thinkutils.log.log import *
 
 logger = setup_custom_logger()
 
@@ -12,3 +10,7 @@ if __name__ == '__main__':
     logger.info("date: %d" % (get_timestamp()))
     logger.info(get_current_time_str())
     logger.info(timestamp2str(get_timestamp()))
+
+    #
+    logger.info(md5_str("123456"))
+    logger.info(md5_file("main.py"))
---
+++ thinkutils/common_utils/think_hashlib.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import hashlib
+import md5
+
+def md5_str(szText):
+    return str(hashlib.md5(szText).hexdigest())
+
+def md5_file(filePath):
+    with open(filePath, 'rb') as fh:
+        m = hashlib.md5()
+        while True:
+            data = fh.read(8192)
+            if not data:
+                break
+            m.update(data)
+        return m.hexdigest()
